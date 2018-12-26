29c9153e6ec3293253d8e51f059800e810d98d77
Evgeniya Ivanova <enginn12@mail.ru>
2018-12-26 19:25:20 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
@@ -6,4 +5,5 @@ const greeting = () => {
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello,  ${userName}!`);
 };
+
 export default greeting;
