29573bbdecc798fc56f81393cc8e65477ea6a872
Evgeniya Ivanova <enginn12@mail.ru>
2018-12-30 00:00:44 +0300

    add progression game

---
+++ src/games/progression.js
@@ -0,0 +1,30 @@
+import { cons } from 'hexlet-pairs';
+import game from '../gameConstructor';
+import getRandomNum from '../utils';
+
+const progressionLength = 10;
+
+const getProgression = (difference) => {
+  let startNumber = getRandomNum(1, 10);
+  const progression = [];
+  for (let i = progressionLength; i > 0; i -= 1) {
+    progression.push(startNumber);
+    startNumber += difference;
+  }
+  return progression;
+};
+
+const generateGameData = () => {
+  const difference = getRandomNum(1, 10);
+  const progression = getProgression(difference);
+  const hiddenElementIndex = getRandomNum(1, 10);
+  const hiddenElement = progression[hiddenElementIndex];
+  const answer = hiddenElement;
+  progression[hiddenElementIndex] = '..';
+  const question = progression.join(' ');
+  return cons(question, String(answer));
+};
+
+const task = 'What number is missing in the progression?';
+
+export default () => game(task, generateGameData);
